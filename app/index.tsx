b0b38d9df0a7b4ddd8cf786d1824c240643be7b6
UP240357 <up240357@alumnos.upa.edu.mx>
2026-03-02 18:15:17 -0600

    add getPokemons

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,23 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  useEffect(() => {
+    //contenido
+    console.log("Entre en pantalla y mori");
+    getPokemons();
+  }, []);
+
+  const getPokemons = async () => {
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+    const res = await fetch(URL, {
+      method: "GET",
+    });
+    console.log(res);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
